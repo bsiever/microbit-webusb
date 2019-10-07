36bd326e1141b220aa5d275ffd7dfe9aed0021fa
Bill Siever <bsiever@gmail.com>
2019-10-07 00:41:42 -0500

    Updates...Partially working...

--- readme.md
+++ readme.md
@@ -44,3 +44,162 @@ The micro:bit retains it's program until it is explicitly re-programmed or the f
 # Credits
 
 * Basic HTML page layout from W3 schools: [https://www.w3schools.com/html/html5_intro.asp](https://www.w3schools.com/html/html5_intro.asp)
+
+
+# Capturing USB Packets on Mac
+
+Based on [https://www.umpah.net/how-to-sniff-usb-traffic-reverse-engineer-usb-device-interactions/](https://www.umpah.net/how-to-sniff-usb-traffic-reverse-engineer-usb-device-interactions/)
+
+Enable virtual port for monitoring:
+
+```
+sudo ifconfig XHC20 up
+```
+
+(`sudo ifconfig XHC20 down` when done)
+
+Use Wireshark for capture.  Filter based on device's location ID (get it from Apple Menu, `About this Mac...`, `System Report`, `USB`, select the device and look at the Loation ID): `usb.darwin.location_id == 0x14200000`
+
+* Start port capture of XH20 interface (wired)
+
+
+# Misc: App Notes / Docs on USB replacing Serial
+
+* [http://ww1.microchip.com/downloads/en/appnotes/doc4322.pdf](http://ww1.microchip.com/downloads/en/appnotes/doc4322.pdf)
+* [https://www.xmos.com/download/AN00124:-USB-CDC-Class-as-Virtual-Serial-Port(2.0.2rc1).pdf](https://www.xmos.com/download/AN00124:-USB-CDC-Class-as-Virtual-Serial-Port(2.0.2rc1).pdf)
+  * Section on USB descriptors helpful
+* [https://github.com/microsoft/pxt/blob/83dcac3c75b8681cd2451919840a297d7e0b3c20/pxtlib/webusb.ts](https://github.com/microsoft/pxt/blob/83dcac3c75b8681cd2451919840a297d7e0b3c20/pxtlib/webusb.ts)
+  * Micro:bit webusb connection 
+
+# Misc: USB Enumeration for Micro:Bit
+
+
+0: USBConfiguration
+
+0: USBInterface
+interfaceClass: 8           // Mass Storage
+interfaceProtocol: 80
+interfaceSubclass: 6
+interfaceNumber: 0
+
+1: USBInterface
+interfaceClass: 2           // CDC
+interfaceProtocol: 1
+interfaceSubclass: 2
+interfaceNumber: 1
+
+2: USBInterface             // CDC - Data 
+interfaceClass: 10
+interfaceProtocol: 0
+interfaceSubclass: 0
+interfaceNumber: 2
+
+3: USBInterface
+interfaceNumber: 3
+
+4: USBInterface
+interfaceNumber: 4
+
+
+# Tracing USB interactions in Chrome
+
+Things to track:
+
+* Device's `claimInterface()`
+* Device's `transferControlOut()`
+* Device's `transferControlIn()`
+* Device's `transferIn()`
+
+```javascript
+
+var trackerLogOn = true;
+
+function addTracker(methodName, object) {
+    var temp = object[methodName];
+    object[methodName] = function() {
+        if(trackerLogOn) {
+            console.log(methodName + ":");
+            console.dir(arguments);
+            console.log("----------------")
+        }
+        // this is a USBDevice object
+        return temp.apply(this, arguments)
+    }
+}
+
+addTracker("claimInterface",USBDevice.prototype)
+addTracker("selectAlternateInterface", USBDevice.prototype)
+addTracker("controlTransferIn", USBDevice.prototype)
+addTracker("controlTransferOut", USBDevice.prototype)
+addTracker("transferIn", USBDevice.prototype)
+addTracker("transferOut", USBDevice.prototype)
+//addTracker("selectConfiguration", USBDevice.prototype)
+addTracker("isochronousTransferIn", USBDevice.prototype)
+addTracker("isochronousTransferOut", USBDevice.prototype)
+
+```
+
+
+
+
+USBDevice.prototype.
+
+
+# Example to `ControlTransferOut`
+
+controlTransferOut: (`set_report`)
+Arguments(2)
+0: {requestType: "class", recipient: "interface", request: 9, value: 512, index: 4}
+index: 4
+recipient: "interface"
+request: 9
+requestType: "class"
+value: 512
+
+controlTransferIn: (`get_report_req`)
+Arguments(2)
+0: {requestType: "class", recipient: "interface", request: 1, value: 256, index: 4}
+1: 64
+
+This is a DAPLink HID request [https://github.com/ARMmbed/DAPLink/blob/b4c90ae660c0067151b22129be350e29cb47a982/test/usb_hid.py](https://github.com/ARMmbed/DAPLink/blob/b4c90ae660c0067151b22129be350e29cb47a982/test/usb_hid.py) (a `set_report_req`)
+
+It looks like this is a DAPLink Serial Wire Out (SWO): [https://arm-software.github.io/CMSIS_5/DAP/html/group__DAP__swo__gr.html](https://arm-software.github.io/CMSIS_5/DAP/html/group__DAP__swo__gr.html)
+
+
+
+
+
+Setup: [https://arm-software.github.io/CMSIS_5/DAP/html/group__DAP__SWD__Configure.html](https://arm-software.github.io/CMSIS_5/DAP/html/group__DAP__SWD__Configure.html)
+
+DAP_SWJ_CLOCK
+[17,128,150,152,0]
+
+DAP_Connect
+[2,0]
+
+DAP_TransferConfigure
+[4, 0, 80, 0, 0, 0]
+
+SWD Configure
+[19, 0]
+
+
+[18, 56, 255, 255, 255, 255, 255, 255, 255]  // DAP_SWJ_Sequence
+[18, 16, 158, 231]
+ [18, 8, 0]
+
+DAP_Transfer
+[5, ...]
+...
+
+[130, 0, 194, 1, 0]      // Set UART Config [https://github.com/ARMmbed/DAPLink/blob/0711f11391de54b13dc8a628c80617ca5d25f070/source/daplink/cmsis-dap/DAP_vendor.c](https://github.com/ARMmbed/DAPLink/blob/0711f11391de54b13dc8a628c80617ca5d25f070/source/daplink/cmsis-dap/DAP_vendor.c)
+[0, 254]                  // DAP_Info / Get Packet Size
+[17, 128, 150, 152, 0]   // DAP_SWJ_Clock
+[2, 0]                  // DAP_Connect Defaults
+[17, 128, 150, 152, 0]   // DAP_SWJ_Clock
+[4, 0, 80, 0, 0, 0]      // DAP_TransferConfigure 
+[19, 0]                  // DAP_SWD_Configure default
+[18, 56, 255, 255, 255, 255, 255, 255, 255]  // DAP_SWJ_Sequence
+
+...
+[5, 0, 1, 2]
--- webusb.js
+++ webusb.js
@@ -2,6 +2,10 @@ console.log("Loading..")
 
 const MICROBIT_VENDOR_ID = 0x0d28
 const MICROBIT_PRODUCT_ID = 0x0204
+const controlTransferGetReport = 0x01;
+const controlTransferSetReport = 0x09;
+const controlTransferOutReport = 0x200;
+const controlTransferInReport = 0x100;
 
 
 Promise.delay = function(duration){
@@ -33,7 +37,7 @@ function recvPacketAsync2() {
 
 function receiveLoop(result) {
 //        console.log('Received: ');
-        console.dir(result)
+        //console.dir(result)
 
         if (result.status != "ok") {
             console.log("USB IN transfer failed")
@@ -43,7 +47,9 @@ function receiveLoop(result) {
         let arr = new Uint8Array(result.data.buffer)
         if(arr.length>0) {
             // Data: Process and get more
-            console.dir(arr)
+            let string =  new TextDecoder("utf-8").decode(arr);
+            console.log(string)
+//            console.dir(arr)
             return recvPacketAsync2().then(receiveLoop)
         } else {
             return Promise.delay(500).then(recvPacketAsync2).then(receiveLoop)
@@ -55,24 +61,150 @@ function connectDevice(device) {
     console.dir(device)
 //            (navigator as any).usb.addEventListener('disconnect', (event: any) => {
 
-// NOTES: 
-//  Works when running Arduino FIRST.  Must be a config / control transfer thing...
-dev = device;
-    device.open()
-        .then(() => {console.log(1) ; return device.selectConfiguration(1)} ) // Select configuration #1 for the device.
-        .then(() => {console.log(2); return device.claimInterface(2)}) // Request exclusive control over interface #2.
-       // .then(() => {console.log(2); device.selectAlternateInterface(2,0)}) // Request exclusive control over interface #2.
-        // .then(() => {console.log(3); return device.controlTransferOut({
-        //                     requestType: 'class',
-        //                     recipient: 'interface',
-        //                     request: 0x22,
-        //                     value: 0x01,
-        //                     index: 0x02})}) // Ready to receive data
-        .then(recvPacketAsync) // Ready to receive data
-        //.then(() => {console.log(4); return device.transferIn(4, 64)}) // Waiting for 64 bytes of data from endpoint #5.
-        .then(recvPacketAsync2)
-        .then(receiveLoop)
+    var transferLoop = function () {
+       // console.log(4)
+//        console.log(data);
+        device.controlTransferOut({
+            requestType: "class",
+            recipient: "interface",
+            request: 9,
+            value: 512,
+            index: 4
+        }, Uint8Array.from([131]))
+//        .then( () => { console.log(5); return device.transferIn(4, 64) })
+        .then( () => { //console.log(5);
+             return device.controlTransferIn( {
+            requestType: "class",
+            recipient: "interface",
+            request: 1,
+            value: 256,
+            index: 4
+          }, 64) })
+//        .then((d) => {console.log(d); console.log(6); return device.transferIn(64,4)})
+        .then( (data) => { 
+           // console.log(7)
+           // console.dir(data)
+            if (data.status != "ok") {
+                console.log("USB IN transfer failed")
+                return Promise.delay(500).then(transferLoop);
+            }
+            let arr = new Uint8Array(data.data.buffer)
+            if(arr.length>0) {
+                // Data: Process and get more
+               // console.log("----------")
+               // console.dir(arr+"")
+                let string =  new TextDecoder("utf-8").decode(arr.slice(2));
+                console.log(string)
+            }
+            return transferLoop();
+        })
         .catch(error => { console.log(error); });
+    }
+// NOTES: 
+var pkt = Uint8Array.from([0,254]); 
+
+//  Works when running Arduino FIRST.  Must be a config / control transfer thing...
+    device.open()
+       .then( () => {console.log(1) ; return device.selectConfiguration(1)})
+//        .then(() => {console.log(2); return device.claimInterface(2)})
+        .then(() => {console.log(2.2); return device.claimInterface(4)})
+        .then(() => device.controlTransferOut({
+            requestType: "class",
+            recipient: "interface",
+            request: 9,
+            value: 512,
+            index: 4
+         }, Uint8Array.from([2, 0])))
+        .then(() => device.controlTransferIn( {
+            requestType: "class",
+            recipient: "interface",
+            request: 1,
+            value: 256,
+            index: 4
+          }, 64) )
+          .then(() =>  device.controlTransferOut({
+            requestType: "class",
+            recipient: "interface",
+            request: 9,
+            value: 512,
+            index: 4
+        }, Uint8Array.from([17, 128, 150, 152, 0])))
+        .then(() =>  device.controlTransferIn( {
+            requestType: "class",
+            recipient: "interface",
+            request: 1,
+            value: 256,
+            index: 4
+          }, 64) )
+          .then(() => device.controlTransferOut({
+            requestType: "class",
+            recipient: "interface",
+            request: 9,
+            value: 512,
+            index: 4
+        }, Uint8Array.from([19, 0] )))
+        .then(() => device.controlTransferIn( {
+            requestType: "class",
+            recipient: "interface",
+            request: 1,
+            value: 256,
+            index: 4
+          }, 64) )
+          .then( () => { console.log(1); return device.controlTransferOut({
+            requestType: "class",
+            recipient: "interface",
+            request: 9,
+            value: 512,
+            index: 4
+        }, Uint8Array.from([130, 0, 194, 1, 0] ))})
+        .then(() => { console.log(2);  return device.controlTransferIn( {
+            requestType: "class",
+            recipient: "interface",
+            request: 1,
+            value: 256,
+            index: 4
+          }, 64) })
+                    //         .then(device.controlTransferOut({
+//             requestType: "class",
+//             recipient: "interface",
+//             request: 9,
+//             value: 512,
+//             index: 4
+//         }, pkt))
+// //        .then( () => { console.log(5); return device.transferIn(4, 64) })
+//         .then( () => { console.log(5); return device.controlTransferIn( {
+//             requestType: "class",
+//             recipient: "interface",
+//             request: 1,
+//             value: 256,
+//             index: 4
+//           }, 64) })         // Request exclusive control over interface #2. (CDC Data)
+        .then(transferLoop)
+        .catch(error => { console.log(error); });
+
+
+
+
+
+//     /////        .then(() => {console.log(1) ; return device.selectConfiguration(1)} ) // Select configuration #1 for the device.
+// // Find interfaceClass 0xFF ; Get the in/out endpoints
+//         .then(() => {console.log(2); return device.claimInterface(2)}) // Request exclusive control over interface #2.
+//         .then(() => {console.log(2); device.selectAlternateInterface(2,0)}) // Request exclusive control over interface #2.
+//         // .then(() => {console.log(3); return device.controlTransferOut({
+//         //                      requestType: 'class',
+//         //                      recipient: 'interface',
+//         //                      request: 0x22,
+//         //                      value: 0x01,
+//         //                      index: 0x02})}) // Ready to receive data
+//         // // readLoop here...
+
+// //                             .then(recvPacketAsync) // Ready to receive data
+//                              // Use "in" Endpoint
+//         .then(() => {console.log(4); return device.transferIn(4, 64)}) // Waiting for 64 bytes of data from endpoint #5.
+// //            .then(...)
+//         //.then(recvPacketAsync2)
+//         .then( (d) => {console.log(5); console.dir(d); receiveLoop(d);})
+//         .catch(error => { console.log(error); });
 }
 
 function getDevices() {
