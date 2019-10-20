26552fd2eec21c834e8b8375505005becccfe05b
Bill Siever <bsiever@gmail.com>
2019-10-20 14:14:11 -0500

    Better API / Callback

--- webusb.js
+++ webusb.js
@@ -24,29 +24,11 @@ const DAPInReportRequest =  {
     index: MICROBIT_DAP_INTERFACE
 }
 
-// Callbacks:
-//   1) onConnect
-//   2) onDisconnect 
-//   3) onData(Graph, Series, Data)
-//   4) onConnectionFailure
+// Callback reasons
+//   "connection failure", "connected", "disconnected", "error"
 
-const uBitCallbacks = {
-    onConnect: function() {},
-    onDisconnect: function() {},
-    onData: function() {}, 
-    onConnectionFailure: function() {}
-}
 
-function uBitSetCallbacks(onConnect, onDisconnect, onData, onConnectionFailure) {
-    if(onConnect)
-        uBitCallbacks.onConnect = onConnect
-    if(onDisconnect)
-        uBitCallbacks.onDisconnect = onDisconnect
-    if(onData)
-        uBitCallbacks.onData = onData
-    if(onConnectionFailure)
-        uBitCallbacks.onConnectionFailure = onConnectionFailure
-}
+
 
 
 // Add a delay() method to promises 
@@ -59,10 +41,8 @@ Promise.delay = function(duration){
     });
 }
 
-var uBitConnectedDevice = null
 
-function uBitConnectDevice(device) {
-
+function uBitOpenDevice(device, callback) {
     function controlTransferOutFN(data) {
         return () => { return device.controlTransferOut(DAPOutReportRequest, data) }
     }
@@ -78,7 +58,6 @@ function uBitConnectDevice(device) {
                 return Promise.delay(500).then(transferLoop);
             }
             // First byte is echo of get UART command
-
 
             let arr = new Uint8Array(data.data.buffer)
             if(arr.length<2)  // Not a valid array: Delay
@@ -98,7 +77,7 @@ function uBitConnectDevice(device) {
 
                 // Deal with line
                 // Parse data and add in time stamp
-                uBitCallbacks.onData(messageToNewline)
+                callback("data", device, messageToNewline)
 
                 buffer = buffer.slice(firstNewline+1)
                 firstNewline = buffer.indexOf("\n")
@@ -107,7 +86,7 @@ function uBitConnectDevice(device) {
             return Promise.delay(5).then(transferLoop);
         })
         // Error here probably means micro:bit disconnected
-        .catch(error => uBitCallbacks.onDisconnect(error));
+        .catch(error => { if(device.opened) callback("error", device, null); device.close(); callback("disconnected", device, null);});
     }
 
     device.open()
@@ -117,34 +96,32 @@ function uBitConnectDevice(device) {
         .then(controlTransferOutFN(Uint8Array.from([0x11, 0x80, 0x96, 0x98, 0]))) // Set Clock: 0x989680 = 10MHz : https://arm-software.github.io/CMSIS_5/DAP/html/group__DAP__SWJ__Clock.html
         .then(controlTransferOutFN(Uint8Array.from([0x13, 0]))) // SWD Configure (1 clock turn around; no wait/fault): https://arm-software.github.io/CMSIS_5/DAP/html/group__DAP__SWD__Configure.html
         .then(controlTransferOutFN(Uint8Array.from([0x82, 0x00, 0xc2, 0x01, 0x00]))) // Vendor Specific command 2 (ID_DAP_Vendor2): https://github.com/ARMmbed/DAPLink/blob/0711f11391de54b13dc8a628c80617ca5d25f070/source/daplink/cmsis-dap/DAP_vendor.c ;  0x0001c200 = 115,200kBps
-        .then( () => { uBitCallbacks.onConnect(); uBitConnectedDevice = device;  return Promise.resolve()}) 
+        .then( () => { callback("connected", device, null); return Promise.resolve()}) 
         .then(transferLoop)
-        .catch(error => uBitCallbacks.onConnectionFailure(error));
+        .catch(error => callback("error", device, error));
 }
 
-function uBitDisconnect() {
-    if(uBitConnectedDevice)
-        uBitConnectedDevice.close()
-    uBitConnectedDevice = null;
+function uBitDisconnect(device) {
+    if(device && device.opened) {
+        device.close()
+    }
 }
 
-
-
-function uBitSend(data) {
-    // Need to sent 0x84 (command), length (including newline), data's caracters, newline
+function uBitSend(device, data) {
+    if(!device.opened)
+        return
+    // Need to sent 0x84 (command), length (including newline), data's characters, newline
     let fullLine = data+'\n'
     let encoded = new TextEncoder("utf-8").encode(fullLine);
     let message = new Uint8Array(1+1+fullLine.length)
     message[0] = 0x84
     message[1] = encoded.length
     message.set(encoded, 2)
-    uBitConnectedDevice.controlTransferOut(DAPOutReportRequest, message) // DAP ID_DAP_Vendor3: https://github.com/ARMmbed/DAPLink/blob/0711f11391de54b13dc8a628c80617ca5d25f070/source/daplink/cmsis-dap/DAP_vendor.c
+    device.controlTransferOut(DAPOutReportRequest, message) // DAP ID_DAP_Vendor3: https://github.com/ARMmbed/DAPLink/blob/0711f11391de54b13dc8a628c80617ca5d25f070/source/daplink/cmsis-dap/DAP_vendor.c
 }
 
-function uBitGetDevices() { 
-    uBitDisconnect()
-
+function uBitConnectDevice(callback) { 
     navigator.usb.requestDevice({filters: [{ vendorId: MICROBIT_VENDOR_ID, productId: 0x0204 }]})
-        .then(  d => uBitConnectDevice(d) )
-        .catch( () => uBitCallbacks.onConnectionFailure("No micro:bit Connected"))
+        .then(  d => { if(!d.opened) uBitOpenDevice(d, callback)} )
+        .catch( () => callback("connection failure", null, null))
 }
